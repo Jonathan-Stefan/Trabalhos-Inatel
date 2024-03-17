a054256eb0e67b4f6d5b6928de95bb8195279b7a
jonathan <jonathan.stefan@inatel.br>
2024-03-17 11:59:38 -0300

    add casos de testes

--- Lab. Eng. Software/ToDoList/src/todo.test.ts
+++ Lab. Eng. Software/ToDoList/src/todo.test.ts
@@ -30,3 +30,62 @@ describe('ToDoList', () => {
     })
   })
 })
+describe('Testing update', () => {
+  test('should update a task in the list', () => {
+    const todoInstance = new ToDoList()
+    todoInstance.add(anyTask)
+    const updatedTask = {
+      title: 'Test_Update',
+      description: 'Test_updated_description',
+      targetDate: '02/02/2025',
+      type: 'Test_updated_type',
+      priority: '2',
+      subTasks: []
+    }
+    todoInstance.updateTask(0, updatedTask)
+    const tasks = todoInstance.getTasks()
+    expect(tasks).toEqual([updatedTask])
+  })
+
+  test('should not update task if index is out of bounds', () => {
+    const todoInstance = new ToDoList()
+    const anyTask = {
+      title: 'any_title',
+      description: 'any_description',
+      targetDate: '01/01/2025',
+      type: 'any_type',
+      priority: '1',
+      subTasks: []
+    }
+    todoInstance.add(anyTask)
+    const updatedTask = {
+      title: 'Test_Update',
+      description: 'Test_updated_description',
+      targetDate: '02/02/2025',
+      type: 'Test_updated_type',
+      priority: '2',
+      subTasks: []
+    }
+    todoInstance.updateTask(1, updatedTask) // Index out of bounds
+    const tasks = todoInstance.getTasks()
+    expect(tasks).toEqual([anyTask])
+  })
+})
+
+describe('Testing remove', () => {
+  test('should remove a task from the list', () => {
+    const todoInstance = new ToDoList()
+    todoInstance.add(anyTask)
+    todoInstance.removeTask(0)
+    const tasks = todoInstance.getTasks()
+    expect(tasks).toEqual([])
+  })
+
+  test('should not remove task if index is out of bounds', () => {
+    const todoInstance = new ToDoList()
+    todoInstance.add(anyTask)
+    todoInstance.removeTask(1) // Index out of bounds
+    const tasks = todoInstance.getTasks()
+    expect(tasks).toEqual([anyTask])
+  })
+})
