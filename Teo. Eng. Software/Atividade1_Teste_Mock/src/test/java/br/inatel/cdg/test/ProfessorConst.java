674b6090123689b68d58693e7711d738f554aacc
jonathan <jonathan.stefan@inatel.br>
2024-03-22 19:25:11 -0300

    add classe que simula Json em formato string

---
+++ Teo. Eng. Software/Atividade1_Teste_Mock/src/test/java/br/inatel/cdg/test/ProfessorConst.java
@@ -0,0 +1,42 @@
+package br.inatel.cdg.test;
+
+public class ProfessorConst {
+        public static String Soned = "{ \"nome\": \"Soned\", \n " +
+                "\"horarioDeAtendimento\": \"10:00 - 11:00\", \n " +
+                "\"periodo\": \"integral\", \n " +
+                "\"sala\": 4, \n " +
+                "\"predio\": [1] }";
+
+        public static String Chris = "{ \"nome\": \"Chris\", \n " +
+                "\"horarioDeAtendimento\": \"19:00 - 19:30\", \n " +
+                "\"periodo\": \"noturno\", \n " +
+                "\"sala\": 9, \n " +
+                "\"predio\": [2] }";
+
+        public static String Yvo = "{ \"nome\": \"Yvo\", \n " +
+                "\"horarioDeAtendimento\": \"18:00 - 19:00\", \n " +
+                "\"periodo\": \"noturno\", \n " +
+                "\"sala\": 18, \n " +
+                "\"predio\": [4] }";
+
+        public static String Marcelo = "{ \"nome\": \"Marcelo\", \n " +
+                "\"horarioDeAtendimento\": \"17:30 - 19:00\", \n " +
+                "\"periodo\": \"noturno\", \n " +
+                "\"sala\": 21, \n " +
+                "\"predio\": [5] }";
+        public static String Guilherme = "{ \"nome\": \"Guilherme\", \n " +
+                "\"horarioDeAtendimento\": \"21:00 - 21:30\", \n " +
+                "\"periodo\": \"noturno\", \n " +
+                "\"sala\": 11, \n " +
+                "\"predio\": [3] }";
+        public static String Estevan = "{ \"nome\": \"Estevan\", \n " +
+                "\"horarioDeAtendimento\": \"13:00 - 14:00\", \n " +
+                "\"periodo\": \"integral\", \n " +
+                "\"sala\": 26, \n " +
+                "\"predio\": [6] }";
+        public static String Invalida = "{ \"nome\": \"Entrada invalida\", \n " +
+                "\"horarioDeAtendimento\": \"-\", \n " +
+                "\"periodo\": \"-\", \n " +
+                "\"sala\": -, \n " +
+                "\"predio\": [-] }";
+}
