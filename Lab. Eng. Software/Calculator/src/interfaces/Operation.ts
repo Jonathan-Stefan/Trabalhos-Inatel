6d27cb36213b03cdecde34317960404a666e1804
jonathan <jonathan.stefan@inatel.br>
2024-04-01 19:45:14 -0300

    add interface operation

---
+++ Lab. Eng. Software/Calculator/src/interfaces/Operation.ts
@@ -0,0 +1,7 @@
+// src/interfaces/Operation.ts
+export interface Operation {
+  somar(x: number, y: number): number
+  subtrair(x: number, y: number): number
+  multiplicar(x: number, y: number): number
+  dividir(x: number, y: number): number
+}
